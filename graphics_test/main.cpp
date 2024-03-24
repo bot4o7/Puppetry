e65b272ac355bf80d2d2322b40da4165fb582318
bot4o7 <m13116919450@163.com>
2024-03-24 16:45:48 +0800

    create "graphics_tset" porject and import Dear ImGui (glfw_opengl as backedns) as project's  GUI

---
+++ graphics_test/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdio>
+
+int main()
+{
+	bool is_GameRunning = false;
+
+	// A typical game-like application taking advantage of GPU rendering would run in a loop:
+	while (is_GameRunning) {
+		// Poll events
+		// Update application/game state
+		// Render contents into a framebuffer
+		// Swap/Present framebuffer to the screen
+		// Wait some time (e.g. 1/60 of a second)
+	}
+
+	return 0;
+}
